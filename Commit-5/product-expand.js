fe23713d16c4a8a2252e53067f0137c849797281
Crashew <119567619+Crashew@users.noreply.github.com>
2023-03-08 00:16:46 +0530

    H 08-03-2023

---
+++ Commit-5/product-expand.js
@@ -0,0 +1,64 @@
+function blurToggle1() {
+
+    let blur= document.getElementById('blur-1');
+    blur.classList.toggle('active');
+
+    let popup= document.getElementById('popup1');
+    popup.classList.toggle('active');
+}
+function blurToggle2() {
+
+    let blur= document.getElementById('blur-2');
+    blur.classList.toggle('active');
+
+    let popup= document.getElementById('popup2');
+    popup.classList.toggle('active');
+}
+function blurToggle3() {
+
+    let blur= document.getElementById('blur-3');
+    blur.classList.toggle('active');
+
+    let popup= document.getElementById('popup3');
+    popup.classList.toggle('active');
+}
+function blurToggle4() {
+
+    let blur= document.getElementById('blur-4');
+    blur.classList.toggle('active');
+
+    let popup= document.getElementById('popup4');
+    popup.classList.toggle('active');
+}
+function blurToggle5() {
+
+    let blur= document.getElementById('blur-5');
+    blur.classList.toggle('active');
+
+    let popup= document.getElementById('popup5');
+    popup.classList.toggle('active');
+}
+function blurToggle6() {
+
+    let blur= document.getElementById('blur-6');
+    blur.classList.toggle('active');
+
+    let popup= document.getElementById('popup6');
+    popup.classList.toggle('active');
+}
+function blurToggle7() {
+
+    let blur= document.getElementById('blur-7');
+    blur.classList.toggle('active');
+
+    let popup= document.getElementById('popup7');
+    popup.classList.toggle('active');
+}
+function blurToggle8() {
+
+    let blur= document.getElementById('blur-8');
+    blur.classList.toggle('active');
+
+    let popup= document.getElementById('popup8');
+    popup.classList.toggle('active');
+}
